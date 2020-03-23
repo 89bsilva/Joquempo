e092a407629a10bfe20fd8aba522c1131eacc9b0
bss89 <brunoss.789@gmail.com>
2020-03-22 21:31:34 -0300

    Design Concluido
    Primeira etapa do HTML e CSS concluida

--- joquempo.js
+++ joquempo.js
@@ -2,6 +2,24 @@
 var app = new Vue({
    el: '#joquempo',
    data: {
-   message: 'Hello Vue!'
+      running: false,
+      round: 1,
+      finalRound: 1,
+      playerName: 'Jogador',
+      playerScore: 0,
+      playerClass: {
+         win: false,
+         lose: false
+      },
+      cpuClass: {
+         win: false,
+         lose: false
+      },
+      cpuScore: 0,
+      stage: 0,
+      resultClass: {
+         win: true,
+         lose: false
+      }
    }
 })
